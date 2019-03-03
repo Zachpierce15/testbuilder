c4529284fe5a1cfb21b86ab207905d0aa5268edf
Zachary <zachary.15pierce@gmail.com>
2019-03-02 18:33:30 -0700

    Undid last three tests to check for bugs

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -154,16 +154,6 @@ describe('Maestro', function() {
      expect(detectNetwork("50183456786754")).to.equal("Maestro");
   });
 
-  it.("This checks if card# equal the right card" , function() {
-    expect(detectNetwork("502012345678")).to.equal("Maestro");
-  });
 
-   it.("This checks if card# equal the right card" , function() {
-    expect(detectNetwork("5038123434567")).to.equal("Maestro");
-  });
-
-    it.("This checks if card# equal the right card" , function() {
-    expect(detectNetwork("6304123456789098765")).to.equal("Maestro");
-  });
 });
 
